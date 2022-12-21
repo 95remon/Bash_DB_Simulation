e328356ec979e883ac891f56f08331c0e63ab5ef
remon <95remon@gmail.com>
2022-12-21 15:32:24 +0200

    Add Main Menu

--- DataBaseSemulation.sh
+++ DataBaseSemulation.sh
@@ -1,8 +1,35 @@
+
 DBsFolder=$(ls -d */ | grep "DBs")
 
 if [[ "$DBsFolder" != *"DBs"* ]]; then
   mkdir DBs
 fi
 
+cd DBs
+MainMenuList=("Create DB" "List DB" "Connect DB" "Drop DB" "Exit")
 
+select Option in "${MainMenuList[@]}"
+do
+    case $Option in
+        "Create DB")
+            Creat_DB
+        ;;
+        "List DB")
+            List_DB
+        ;;
+        "Connect DB")
+            Connect_DB
+        ;;
+        "Drop DB")
+            Drop_DB
+        ;;
+        "Exit")
+            cd ..
+            exit 0
+        ;;
+        *)
+            echo "kindly select number again"
+        ;;
+    esac
+done
 
